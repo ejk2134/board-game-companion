9b124f07859bf97b0f387bbbe980c6cfec09dcad
Evan Kearney <evanjkearney@gmail.com>
2024-08-08 15:54:35 -0500

    Added react fast refresh to preserve react state on hot reload

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,14 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const webpack = require('webpack')
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
-];
+  new webpack.HotModuleReplacementPlugin(),
+  new ReactRefreshPlugin(),
+]
